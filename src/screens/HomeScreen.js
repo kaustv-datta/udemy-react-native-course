ff9b1377bb212f337acdc8275a2312d4b323e4fb
Kaustav <kaustv.datta@gmail.com>
2020-05-06 22:27:04 +0200

    CounterScreen + ColorScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -22,6 +22,18 @@ const HomeScreen = ({navigation}) => {
        navigation.navigate('Image');
      }}
      />
+     <Button
+     title="Go to Counter Demo"
+     onPress={() => {
+       navigation.navigate('Counter');
+     }}
+     />
+     <Button
+     title="Go to Color Demo"
+     onPress={() => {
+       navigation.navigate('Color');
+     }}
+     />
   </View>);
 };
 
