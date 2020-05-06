d1dbe4cde26ffa65feab4d81149442ba9e5ab431
Kaustav <kaustv.datta@gmail.com>
2020-05-07 00:18:01 +0200

    CounterScreen update reducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,23 +1,30 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'increase_counter':
+            return state + 1;
+        case 'decrease_counter':
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
 const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, 0);
 
     return <View>
         <Button 
             title="Increase"
-            onPress={() => {
-                setCounter(counter + 1);
-            }}
+            onPress={() => dispatch({type: 'increase_counter'})}
         />
         <Button
             title="Decrease"
-            onPress={() => {
-                setCounter(counter - 1);
-            }}
+            onPress={() => dispatch({type: 'decrease_counter'})}
         />
-        <Text>Current Count: {counter}</Text>
+        <Text>Current Count: {state}</Text>
     </View>
 };
 
